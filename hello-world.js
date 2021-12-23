e96915cae490101b7e3cfb83c792bb8955ea7b99
Bharathula Prasad <KrishnaPrasad.Bharathula@gmail.com>
2021-12-23 12:16:24 +0100

    async call example

--- hello-world.js
+++ hello-world.js
@@ -1,5 +1,7 @@
 
 const logger=require('./logger');
+const fs=require('fs');
+const { Console } = require('console');
 function helloworld(name)
 {
    console.log('Hello world ' + name); // global
@@ -10,3 +12,10 @@ helloworld('krishna ');
 logger.log('Krishna ');
 
 logger.memoryPrint();
+
+// read current directory in asynchronous way
+fs.readdir('./', function(err,files) {
+   if(err) console.log('Error',error);
+   else console.log('Files', files);
+
+});
